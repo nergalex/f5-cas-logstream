9346e1689ac78885826746c8d8c0e9a8979be912
Alexis <al.dacosta@f5.com>
2021-01-20 10:49:18 +0100

    decode

--- wsgi.py
+++ wsgi.py
@@ -472,9 +472,10 @@ class Forward(Resource):
         # Events
         else:
             print("Events")
+            print("%s" % request.data.decode())
             data_json = request.get_json(force=True, silent=True)
             if data_json is None:
-                data_json = json.loads(request.data)
+                data_json = json.loads(request.data.decode())
             cas.append_events(data_json)
             return {'msg': 'security event OK'}
 
